11da12d0f9b9e659bc6faba05b3243e6d4afd090
Tutarat <Tutarat@yandesx.ru>
2024-05-03 12:33:38 +0300

    + 2 methods

--- src/main/java/ru/tinkoff/qa/CollectionSorter.java
+++ src/main/java/ru/tinkoff/qa/CollectionSorter.java
@@ -1,7 +1,6 @@
 package ru.tinkoff.qa;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class CollectionSorter<T> {
     /**
@@ -12,9 +11,17 @@ public class CollectionSorter<T> {
      * @return Словарь значений и их вхождения в коллекцию
      */
     public Map<T, Integer> occurrencesInList(List<T> list) {
-        // тут код
-        return null;
+        Map<T, Integer> countMap = new HashMap<>();
+        list.forEach(n -> {
+            if (countMap.containsKey(n)) {
+                countMap.put(n, countMap.get(n) + 1);
+            } else {
+                countMap.put(n, 1);
+            }
+        });
+       return countMap;
     }
+
 
     /**
      * Вернуть коллекцию без дубликатов, только уникальные значения.
@@ -23,9 +30,27 @@ public class CollectionSorter<T> {
      * @return Коллекция только с уникальными значениями.
      */
     public List<T> deleteDuplication(List<T> list) {
+        List<T> countList = new ArrayList<>();
+        list.forEach(n -> {
+            if (!countList.contains(n)) {
+                countList.add(n);
+            }
+        });
+        return countList;
         // тут код
-        return null;
     }
+//
+//    public List<T> deleteDuplicationCircle(List<T> list) {
+//        List<T> countList = new ArrayList<>();
+//        for (var i = 0; i < countList.size(); i++)
+//        {
+//            if (!countList.contains(countList.get(i))) {
+//                countList.add(countList.get(i));
+//            }
+//        };
+//        return countList;
+//        // тут код
+//    }
 
     /**
      * Вернуть только дубликаты и их количество в коллекции.
@@ -34,7 +59,13 @@ public class CollectionSorter<T> {
      * @return Словарь дубликатов и их количества
      */
     public Map<T, Integer> duplicationCount(List<T> list) {
+        Map<T, Integer> countDub = new HashMap<>();
+        list.forEach(n -> {
+            if (countDub.containsKey(n)) {
+                countDub.put(n, countDub.get(n) + 1);
+            }
+        });
+        return countDub;
         // тут код
-        return null;
     }
 }
